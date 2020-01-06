57c0f0dc3f892669e8bcb961fa2946a30d5783ca
sniper2x5 <58059019+sniper2x5@users.noreply.github.com>
2020-01-06 09:04:24 -0500

    Update sumo.py

--- sumo.py
+++ sumo.py
@@ -36,13 +36,13 @@ y1=centerY
 x1_dir=0
 y1_dir=0
 x1_right=False
+x1_left=False
 def char1 (x1,y1):
     """char1 (x1,y1) - creates char1 at given coordinates"""
     pygame.draw.circle(screen, colorRed, (x1,y1),xR)
 print (x1)
 print (centerX)
-if x1_right==True:
-    x1_dir+2
+
     
 
     
@@ -54,12 +54,19 @@ while True:
         if event.type== QUIT:
             pygame.quit()
             sys.exit()
+        
+        while x1_right==True:
+            x1_dir+=1
+        while x1_left==True:
+            x1_dir-=1
+            
         if event.type==KEYDOWN:
             if event.key==K_d:
                 x1_dir+=1
                 x1_right=True
             if event.key==K_a:
                 x1_dir-=1
+                x1_left=True
             if event.key==K_w:
                 y1_dir-=1
             if event.key==K_s:
@@ -67,6 +74,11 @@ while True:
         if event.type==KEYUP:
             if event.key==K_d:
                 x1_right=False
+            if event.key==K_a:
+                x1_left=False
+
+        
+        print(x1_dir)
 
             
 
